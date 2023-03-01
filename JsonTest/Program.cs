23829956069e1ea2eb4c42f576e8c03bfc6a1fb7
chicken10dys <chicken@10dys.ca>
2023-03-01 16:25:36 -0500

    Initial commit

---
+++ JsonTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JsonTest
+{
+    public class JsonData
+     {
+         public string? input { get; set; }
+     }
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            string jsonString;
+            
+            if (File.Exists("Input.json"))
+            {
+                jsonString = File.ReadAllText("Input.json");
+                JsonData jsonData = JsonSerializer.Deserialize<JsonData>(jsonString);
+                Console.WriteLine($"{jsonData?.input}");
+            }
+
+            else
+            {
+                input = Console.ReadLine();
+                var JsonData = new JsonData
+                {
+                    input = input
+                };
+                jsonString = JsonSerializer.Serialize(JsonData);
+    
+                Console.WriteLine(jsonString);
+                File.WriteAllText("Input.json", jsonString);
+            }
+            
+        }
+    }
+
+    
+}
